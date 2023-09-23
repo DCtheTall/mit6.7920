a3c77b1a66d703d13bf7e2abc147a0d2bb90d5f6
DCtheTall <dylancutler@google.com>
2023-09-23 10:23:53 -0400

    LQR example

--- src/01_linear_quadratic_resolver.py
+++ src/01_linear_quadratic_resolver.py
@@ -9,28 +9,33 @@ from control import lqr
 
 
 if __name__ == '__main__':
-    timestep = 1.0
-
     # Flying drone example
     # Want to keep the drone level at height zero.
 
+    # Updates every 5 seconds
+    timestep = 5.0
+
     # Dynamics matrices
     # x_t+1 = A @ x_t + B @ u_t
-    # In this case x is a 2D vector and u is a scalar
+    # In this case x is a 2D vector and u is a scalar representing
+    # vertical force on the drone.
     A = np.array([[1.0, timestep], [0.0, 1.0]])
     B = np.array([[0.5 * timestep ** 2], [timestep]])
 
     # State cost matrix
     # x^T @ Q @ x
+    # Increase first element to make distance from desired height more
+    # constly.
+    # Increase the last to make high velocity more costly.
     Q = np.array([[1.0, 0.0], [0.0, 0.01]])
 
-    # Action cost, increase to make moving the drone more costly.
-    R = 1e3
+    # Action cost, increase to acceleration more costly.
+    R = 1e6
 
     K, S, _  = lqr(A, B, Q, R)
     print('State feedback gains:', K)
-    x = np.array([2.0, 3.0])
-    print('Optimal action for state (2, 3):', -K @ x)
+    x = np.array([20, 0])
+    print('Optimal action for state (20, 0):', -K @ x)
     x_prime = (A - B @ K) @ x
     print('Next state:', x_prime)
     print('Next optimal action:', -K @ x_prime)
