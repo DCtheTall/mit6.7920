3bf10d8645a3e25ab7101f5db7a4dd5b8e239265
DCtheTall <dylancutler@google.com>
2023-09-23 11:04:26 -0400

    Policy iteration example

--- src/02_policy_iteration.py
+++ src/02_policy_iteration.py
@@ -1,6 +1,6 @@
 """
-Implementation of value iteration algorithm
-===========================================
+Implementation of policy iteration algorithm
+============================================
 
 """
 
@@ -15,7 +15,7 @@ def get_reward(s):
     return 0.0
 
 
-def value_iteration(S, A, R, γ, V):
+def policy_iteration(S, R, γ, V, π, A):
     """Value iteration algorithm implementation
     
     Complexity: O(S^2 * A)
@@ -23,24 +23,37 @@ def value_iteration(S, A, R, γ, V):
     n_iter = 0
     while True:
         n_iter += 1
-        V_prime = update_value_function(S, A, R, γ, V)
+        # Policy evaluation
+        V_prime = evaluate_policy(S, R, γ, V, π)
         if all(np.isclose(V[s], V_prime[s]) for s in S):
             break
-        else:
-            V = V_prime
-    return V, n_iter
+        V = V_prime
+
+        # Policy improvement
+        π = update_policy(S, R, γ, V, A)
+    return V, π, n_iter
 
 
-
-def update_value_function(S, A, R, γ, V):
-    """One round of the value function update step
-    
-    Complexity: O(S^2 * A)
-    """
+def evaluate_policy(S, R, γ, V, π):
     return {
-        s: max(bellman_operator(S, R, γ, V, s, a) for a in A)
+        s: bellman_operator(S, R, γ, V, s, π[s])
         for s in S
     }
+
+
+def update_policy(S, R, γ, V, A):
+    """Update the policy using the newly computed value function, V
+    
+    π[s] = argmax(bellman_operator(R, γ, V, s, a) for a in A)
+    """
+    π = {}
+    for s in S:
+        possible_actions = {
+            a: bellman_operator(S, R, γ, V, s, a)
+            for a in A
+        }
+        π[s] = max(possible_actions, key=possible_actions.get)
+    return π
 
 
 def bellman_operator(S, R, γ, V, s, a):
@@ -91,13 +104,17 @@ if __name__ == '__main__':
     # Discount factor
     γ = 0.75
 
-    # Apply value iteration
-    V_opt, n_iter = value_iteration(S, A, R, γ, V)
+    # Initialize stationary policy
+    π = {s: 'Right' for s in S}
+
+    # Apply policy iteration
+    V_opt, π_opt, n_iter = policy_iteration(S, R, γ, V, π, A)
 
     # Display results
     print('Converged after', n_iter, 'iterations')
+    print('Optimal value function:')
     print(V_opt)
-    print(
-        'Best first action:',
-        'Up' if V_opt[(0, 1)] > V_opt[(1, 0)] else 'Right'
-    )
+    print('Optimal policy:')
+    print(π_opt)
+    print('Best first action:', π_opt[(0, 0)])
+
