280b1cb882c5095dabcde654b5dccc1929a32f33
DCtheTall <dylancutler@google.com>
2023-11-06 20:39:37 -0500

    TRPO up to conjugate gradient

--- src/15_trpo.py
+++ src/15_trpo.py
@@ -1,29 +1,7 @@
 """
-Implementation of Natural Policy Gradients (NPG)
-================================================
-NPG improves on policy gradient methods by taking into account the geometry
-of the parameter space you are training a policy with.
-
-It does so by multiplying the gradients by the inverse Fisher information
-matrix, the Hessian of the KL-divergence between the current and candidate
-policy. It can be shown that the Fisher matrix can be approximated using
-
-E(∇log(π(a|s)) x ∇log(π(a|s)))
-
-i.e. the expectation of the outer product of the policy gradient.
-It approximates the expectation with the empirical mean of the outer
-product of gradients over all samples.
-
-This algorithm is able to converge to a decent policy after just 1,000
-trajectories, less than 20% of what REINFORCE required.
-
-Result:
--------
-Optimal policy:
-Action.Up	 Action.Up	 Action.Right	 Action.Right
-Action.Left	 Action.Left	 Action.Left	 Action.Right
-Action.Left	 Action.Left	 Action.Left	 Action.Down
-Action.Left	 Action.Left	 Action.Left	 Action.Down
+Implementation of Trust Region Policy Optimization (TRPO)
+=========================================================
+Note: One difference TRPO paper authors mention is they use actual KL-divergence Hessian for NPG
 
 """
 
@@ -31,25 +9,27 @@ import flax.linen as nn
 import jax
 import jax.numpy as jnp
 import numpy as np
-from util.display import print_grid
 from util.gridworld import GridWorld
+from util.display import print_grid
 from util.jax import MLP, create_sgd_train_state
 
 jax.config.update('jax_enable_x64', True)
 
 
 N_FEATURES = 8
-N_HIDDEN_FEATURES = 4 * N_FEATURES
+N_HIDDEN_FEATURES = 2 * N_FEATURES
 N_HIDDEN_LAYERS = 2
 N_ACTIONS = 4
-LEARNING_RATE = 1e-3
-TRAIN_STEPS = 100
+LEARNING_RATE = 1e-2
+TRAIN_STEPS = 1
 N_TRAJECTORIES_PER_STEP = 10
+N_VALUE_ESTIMATE_ITERATIONS = 10
 MAX_STEPS_PER_TRAJECTORY = 100
-N_VALUE_ESTIMATE_ITERATIONS = 100
+CONJUGATE_GRADIENT_THRESHOLD = 1e-2
+MAX_CONJUGATE_GRADIENT_STEPS = 10
 
 
-def natural_policy_gradient(env, γ, λ, δ):
+def trpo(env, γ, λ, δ):
     # Initialize policy net
     π_net = PolicyNet(hidden_dim=N_HIDDEN_FEATURES,
                       n_layers=N_HIDDEN_LAYERS)
@@ -89,8 +69,18 @@ def natural_policy_gradient(env, γ, λ, δ):
                 continue
             grads = policy_gradient(π_state, np.array(dt), np.array(x),
                                     np.array(a_idx))
-            grads = natural_gradients(grads, δ, np.array(dt))
-            π_state = π_state.apply_gradients(grads=grads)
+            π_prime_state = π_state.apply_gradients(
+                # Negative sign because NN opt does gradient descent
+                grads=jax.tree_util.tree_map(lambda x: -x, grads))
+            q_logits = π_prime_state.apply_fn({'params': π_prime_state.params},
+                                              np.array(x))
+            F = fisher_information_matrix(π_state, q_logits, np.array(x))
+            grads = natural_gradient(F, grads)
+            # TODO use conjugate gradient method to approx natural grad
+            # TODO natural gradient update
+            # TODO check update is in trust region
+            # grads = natural_gradients(grads, δ, np.array(dt))
+            # π_state = π_state.apply_gradients(grads=grads)
     return π_state
 
 
@@ -163,77 +153,102 @@ def td_lambda_learning_rate(t):
 
 
 @jax.jit
-def policy_gradient(π_state, dts, xs, a_idxs):
-    def loss_fn(params, r, x, a_idx):
+def policy_gradient(π_state, r, x, a_idx):
+    def loss_fn(params):
         a_logits = π_state.apply_fn({'params': params}, x)
         a = jnp.take_along_axis(a_logits,
                                 jnp.expand_dims(a_idx, axis=-1),
                                 axis=0)
-        return jax.lax.cond(
-                r < 0.0,
-                lambda: -r * jnp.sum(jnp.log(1.0 - a)),
-                lambda: r * jnp.sum(jnp.log(a)),
-            )
-    return jax.vmap(
-        lambda dt, x, a_idx: jax.grad(loss_fn)(π_state.params, dt, x, a_idx)
-    )(dts, xs, a_idxs)
+        return jnp.mean(
+            jax.vmap(
+                lambda r_i, a_i: jax.lax.cond(
+                    r_i < 0.0,
+                    lambda: -r_i * jnp.log(1.0 - a_i),
+                    lambda: r_i * jnp.log(a_i),
+                )
+            )(r, a)
+        )
+    return jax.grad(loss_fn)(π_state.params)
 
 
-def natural_gradients(grads, δ, dt):
-    # Unravel gradient trees into flat vectors
+@jax.jit
+def fisher_information_matrix(π_state, q_logits, x):
+    def kl_divergence(params):
+        p_logits = π_state.apply_fn({'params': params}, x)
+        return jnp.sum(p_logits * jnp.log(p_logits / q_logits))
+    return jax.hessian(kl_divergence)(π_state.params)
+
+
+def natural_gradient(F, grads):
+    # Unravel gradient tree into a flat vector
     g_leaves, g_tree = jax.tree_util.tree_flatten(grads)
-    n = g_leaves[0].shape[0]
-    g_flat = [[] for _ in range(n)]
+    g_flat = []
     for leaf in g_leaves:
-        for i, grad in enumerate(leaf):
-            g_flat[i].extend(grad.flatten())
-    g_flat = np.array(g_flat)
-    g_nat = compute_natural_gradient(g_flat, δ, dt)
-
+        g_flat.extend(leaf.flatten())
+    g_flat = np.array(g_flat, dtype=np.float64)
+    # Unravel Hessian of KL-divergence into a matrix
+    F_leaves, _ = jax.tree_util.tree_flatten(F)
+    F_flat = []
+    for i, g_leaf in enumerate(g_leaves):
+        start = i * len(g_leaves)
+        end = start + len(g_leaves)
+        n_dims = len(g_leaf.shape)
+        f_flat = [[] for _ in range(np.prod(g_leaf.shape))]
+        for F_leaf in F_leaves[start:end]:
+            F_leaf = F_leaf.reshape([-1] + [np.prod(F_leaf.shape[n_dims:])])
+            for i, f_leaf in enumerate(F_leaf):
+                f_flat[i].extend(f_leaf)
+        F_flat.extend(f_flat)
+    F_flat = np.array(F_flat, dtype=np.float64)
+    g_nat = conjugate_gradient(F_flat, g_flat)
     # Re-ravel gradients into PyTree
     i = 0
     unflatten_input = []
     for shape in [x.shape for x in g_leaves]:
-        shape = shape[1:]
         di = np.prod(shape)
         unflatten_input.append(g_nat[i:i+di].reshape(shape))
         i += di
     return jax.tree_util.tree_unflatten(g_tree, unflatten_input)
 
 
+def conjugate_gradient(F, g, Ɛ=CONJUGATE_GRADIENT_THRESHOLD):
+    """
+    Use conjugate gradients to solve:
+
+    (F^T @ F) @ x = F^T @ b
+
+    We multiply by F^T on the right since F is not guaranteed
+    to be positive definite.
+
+    """
+    # Initial guess: natural gradient == normal gradient
+    x = g
+    A = F.transpose() @ F
+    b = F.transpose() @ g - A @ x
+    b_l1 = np.sum(np.abs(b))
+    r = b  # No Ax term since x is zero vector
+    p = r
+    n_iter = 0
+    for _ in range(MAX_CONJUGATE_GRADIENT_STEPS):
+        n_iter += 1
+        x, r_prime = conjugate_gradient_step(A, r, p, x)
+        error = np.dot(r_prime, r_prime) ** 0.5
+        if error < Ɛ * b_l1:
+            break
+        β = np.dot(r_prime, r_prime) / np.dot(r, r)
+        p = r + β * p
+        r = r_prime
+    return x
+
+
 @jax.jit
-def compute_natural_gradient(g_flat, δ, dt):
-    F = jnp.mean(jnp.array([jnp.outer(g, g) for g in g_flat]), axis=0)
-    F_inv = jnp.linalg.pinv(F)
+def conjugate_gradient_step(A, r, p, x):
+    p_prime = jnp.matmul(A, p)
+    α = jnp.dot(r, r) / jnp.dot(p, p_prime)
+    x_prime = x + α * p
+    r_prime = r - α * p_prime
+    return x_prime, r_prime
 
-    # Scale summed gradient
-    g_nat = jnp.einsum('ab,cb->ca', F_inv, g_flat)
-
-    # Scale step size
-    scale = jnp.mean(jnp.einsum('ab,ab->a', g_nat, g_flat))
-    step_size = jnp.sqrt(2 * δ / (jnp.abs(scale) + 1e-12))
-    g_nat *= step_size
-
-    # Negative sign for NN optimizer
-    return -jnp.sum(g_nat, axis=0)
-
-
-def optimal_policy(π_state, S, A, ϕ):
-    π = {}
-    for s in S:
-        x = ϕ[s]
-        a_logits = π_state.apply_fn({'params': π_state.params}, np.array([x]))[0]
-        π[s] = A[np.argmax(a_logits)]
-    return π
-
-
-def optimal_value_function(V_state, S, ϕ):
-    V = {}
-    for s in S:
-        x = ϕ[s]
-        v = V_state.apply_fn({'params': V_state.params}, np.array([x]))[0]
-        V[s] = v
-    return V
 
 
 if __name__ == '__main__':
@@ -243,11 +258,7 @@ if __name__ == '__main__':
     γ = 0.75
     λ = 0.6
 
-    # Step size scale
+    # Trust region size
     δ = 1e-2
 
-    π_state = natural_policy_gradient(env, γ, λ, δ)
-    π_opt = optimal_policy(π_state, env.S, env.A, env.ϕ)
-
-    print('Optimal policy:')
-    print_grid(π_opt)
+    π_state = trpo(env, γ, λ, δ)
