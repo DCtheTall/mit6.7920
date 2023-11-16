b1835857f02a525b4e002b9ef12ed6c7047a9d1c
DCtheTall <dylancutler@google.com>
2023-11-15 23:46:21 -0500

    Implement Monte Carlo search trees

--- src/util/display.py
+++ src/util/display.py
@@ -9,4 +9,4 @@ from typing import Any, Dict, Tuple
 def print_grid(X: Dict[Tuple[int, int], Any],
                size: int = 4):
     for y in range(size - 1, -1, -1):
-        print(*(str(X[(x, y)]) + '\t' for x in range(size)))
+        print(*(str(X.get((x, y), None)) + '\t' for x in range(size)))
